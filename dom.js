00a9120e58b89b615de9de76199bfd15d56c11f0
NEERAJ SINGH RAJPUT <nsinghrajput30@gmail.com>
2023-03-20 12:52:02 +0530

    seller admin page

--- dom.js
+++ dom.js
@@ -1,18 +1,21 @@
 var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+var eitemList = document.getElementById('eitems');
+var fitemList = document.getElementById('fitems');
+var sitemList = document.getElementById('sitems');
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
-itemList.addEventListener('click', removeItem);
-itemList.addEventListener('click', editItem);
+eitemList.addEventListener('click', removeItem);
+fitemList.addEventListener('click', removeItem);
+sitemList.addEventListener('click', removeItem);
 var myobj={};
 var myobj1;
 // Add item
 function addItem(e){
   e.preventDefault();
-  var newItem = document.getElementById('name').value;
-  var newItem1 = document.getElementById('email').value;
-  var newItem2 = document.getElementById('number').value;
+  var newItem = document.getElementById('price').value;
+  var newItem1 = document.getElementById('category').value;
+  var newItem2 = document.getElementById('pname').value;
   var li = document.createElement('li');
   li.className = 'list-group-item';
   li.appendChild(document.createTextNode(newItem));
@@ -22,19 +25,23 @@ function addItem(e){
   li.appendChild(document.createTextNode(newItem2));
   li.appendChild(document.createTextNode(" "));
   var deleteBtn = document.createElement('button');
-  var editBtn = document.createElement('button');
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-  editBtn.className = 'btn btn-warning btn-sm edit';
   deleteBtn.appendChild(document.createTextNode('Delete'));
-  editBtn.appendChild(document.createTextNode('Edit'))
   li.appendChild(deleteBtn);
-  li.appendChild(editBtn);
-  itemList.appendChild(li);
-  myobj={
-    name:`${newItem}`,
-    email:`${newItem1}`,
-    number:`${newItem2}`
+  let val=document.getElementById('category').value
+  if(val == 'Electronic Items'){
+    eitemList.appendChild(li);  
+  }else if(val == 'Food Items'){
+    fitemList.appendChild(li);
+  }else{
+    sitemList.appendChild(li);
   }
+  myobj={
+    price:`${newItem}`,
+    category:`${newItem1}`,
+    pname:`${newItem2}`
+  }
+  console.log(myobj)
   axios.post('https://crudcrud.com/api/42e0cd4e9f75460b89d124df4690b29e/bookappointment', myobj)
        .then(res => console.log(res))
        .catch(err => console.log(err))
@@ -46,15 +53,15 @@ window.addEventListener('DOMContentLoaded', ()=> {
           showUseronScreen(res.data[i])
           let obj=JSON.stringify(res.data[i])
           let myobj=JSON.parse(obj)
-          localStorage.setItem(myobj.number,obj)
+          localStorage.setItem(myobj.price,obj)
           console.log(myobj)
         }
        })
 })
 function showUseronScreen(obj){
-  let newItem= obj.name;
-  let newItem1=obj.email;
-  let newItem2=obj.number;
+  let newItem= obj.price;
+  let newItem1=obj.category;
+  let newItem2=obj.pname;
   var li = document.createElement('li');
   li.className = 'list-group-item';
   li.appendChild(document.createTextNode(newItem));
@@ -64,14 +71,17 @@ function showUseronScreen(obj){
   li.appendChild(document.createTextNode(newItem2));
   li.appendChild(document.createTextNode(" "));
   var deleteBtn = document.createElement('button');
-  var editBtn = document.createElement('button');
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-  editBtn.className = 'btn btn-warning btn-sm edit';
-  deleteBtn.appendChild(document.createTextNode('Delete'));
-  editBtn.appendChild(document.createTextNode('Edit'))
+  deleteBtn.appendChild(document.createTextNode('Delete Product'));
   li.appendChild(deleteBtn);
-  li.appendChild(editBtn);
-  itemList.appendChild(li);
+  let val=obj.category
+  if(val == 'Electronic Items'){
+    eitemList.appendChild(li);  
+  }else if(val == 'Food Items'){
+    fitemList.appendChild(li);
+  }else{
+    sitemList.appendChild(li);
+  }
 }
 
 
@@ -92,30 +102,15 @@ function removeItem(e){
         .catch(err => console.log(err));
         localStorage.removeItem(num[0]);
         var li=e.target.parentElement;
-        itemList.removeChild(li);
+        let val=obj.category;
+        if(val == 'Electronic Items'){
+          eitemList.removeChild(li);  
+        }else if(val == 'Food Items'){
+          fitemList.removeChild(li);
+        }else{
+          sitemList.removeChild(li);
+        }
       }
   }
-}
-
-function editItem(e){
-    if(e.target.classList.contains('edit')){
-        let x=e.target.parentElement.textContent
-        var num = `${x}`.match(/\d+/g);
-        let obj = localStorage.getItem(`${num[0]}`)
-        if(obj==undefined){
-          obj=localStorage.getItem(`${num[1]}`)
-        }
-        obj=JSON.parse(obj)
-        let id=obj._id;
-        document.getElementById('name').value=obj.name;
-        document.getElementById('email').value = obj.email;
-        document.getElementById('number').value =obj.number;
-        axios.delete(`https://crudcrud.com/api/42e0cd4e9f75460b89d124df4690b29e/bookappointment/${id}`)
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
-        localStorage.removeItem(num[0]);
-        var li=e.target.parentElement;
-        itemList.removeChild(li);
-    }
 }
 
